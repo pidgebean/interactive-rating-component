b79e05cbd21bd9868fd0ad3c9e616e6fa92d2424
Elizabeth Bartholomew <lizzy.bartholomew@gmail.com>
2023-07-07 11:02:48 -0600

    updated

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 let chosenRating = document.getElementById("chosen-rating");
 
 function whichRating(){
+    document.getElementById("form-2").setAttribute('style', 'display:block');
+    document.getElementById("form-1").setAttribute('style', 'display:none');
 
     if (document.getElementById("star-1").checked) {
-        chosenRating.innerHTML = document.getElementById("label-1").value;
+        chosenRating.innerHTML = document.getElementById("label-1").innerHTML;
     }
     if (document.getElementById("star-2").checked) {
-        chosenRating.innerHTML = document.getElementById("label-2").value;
+        chosenRating.innerHTML = document.getElementById("label-2").innerHTML;
     }
     if (document.getElementById("star-3").checked) {
-        chosenRating.innerHTML = document.getElementById("label-3").value;
+        chosenRating.innerHTML = document.getElementById("label-3").innerHTML;
     }
     if (document.getElementById("star-4").checked) {
-        chosenRating.innerHTML = document.getElementById("label-4").value;
+        chosenRating.innerHTML = document.getElementById("label-4").innerHTML;
     }
     if (document.getElementById("star-5").checked) {
-        chosenRating.innerHTML = document.getElementById("label-5").value;
+        chosenRating.innerHTML = document.getElementById("label-5").innerHTML;
     }
     
 }
